ad24ca7b90de6ffa52cf89cf359da62fb404522e
lixiang <675196450@qq.com>
2020-11-02 17:14:02 +0800

    增加

--- src/com/lixiang/dynamicproxy/jdk/jdkmain.java
+++ src/com/lixiang/dynamicproxy/jdk/jdkmain.java
@@ -1,7 +1,11 @@
 package com.lixiang.dynamicproxy.jdk;
 
 import net.sf.cglib.proxy.Enhancer;
+import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
@@ -29,8 +33,11 @@ public class jdkmain {
         System.out.println(userServiceProxy.getName(1));
         //System.out.println(userServiceProxy.getAge(1));
 
+        UserService userServiceProxy1 = create(UserService.class);
+        System.out.println(userServiceProxy1.getName(1));
 
-        //基于cglib的动态代理，不需要让类实现
+
+       /* //基于cglib的动态代理，不需要让类实现
         // 通过CGLIB动态代理获取代理对象的过程
         Enhancer enhancer = new Enhancer();
         // 设置enhancer对象的父类
@@ -40,8 +47,25 @@ public class jdkmain {
         // 创建代理对象
         UserServiceCglib proxy= (UserServiceCglib)enhancer.create();
         // 通过代理对象调用目标方法
-        System.out.println(proxy.getAge(30));
+        System.out.println(proxy.getAge(30));*/
     }
 
+    public static <T> T create(final Class<T> service) {
+        return (T) Proxy.newProxyInstance(
+                service.getClassLoader(),
+                new Class<?>[]{service},
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, @Nullable Object[] args)
+                            throws Throwable {
+                        // If the method is a method from Object then defer to normal invocation.
+                        if (method.getDeclaringClass() == Object.class) {
+                            return method.invoke(this, args);
+                        }
+                        System.out.println("asdasdsa");
+                        return "200";
+                    }
+                });
+    }
 
 }
